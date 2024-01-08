4210d230855a024e2060060a5ddd76ca76731994
Niema Moshiri <niemamoshiri@gmail.com>
2024-01-07 17:42:38 -0800

    Removed more duplicates

--- helper/remove_duplicates.py
+++ helper/remove_duplicates.py
@@ -4,13 +4,14 @@ Try to remove duplicate folders
 '''
 from glob import glob
 from shutil import rmtree
+DIGITS = {str(i) for i in range(10)}
 
 # main program
 if __name__ == "__main__":
     folders = set(glob('*'))
     to_delete = set()
     for folder in folders:
-        if (folder.endswith('-1') or folder.endswith('-2')) and folder[:-2] in folders:
+        if folder.split('-')[-1] in DIGITS and '-'.join(folder.split('-')[:-1]) in folders:
             to_delete.add(folder)
     if len(to_delete) == 0:
         print("No duplicate folders detected"); exit()
